aef5ae13ded85b03934aa2e18b1de5fa2e306f94
ScuttiFrancesco <frarapto87@gmail.com>
2025-05-31 12:36:34 +0200

    button

---
+++ src/app/components/button.component.ts
@@ -0,0 +1,99 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <button
+      [type]="type()"
+      [disabled]="disabled()"
+      [class]="cssClass()"
+      [style.width.vw]="width()"
+      [style.height.vh]="height()"
+      [style.cursor]="disabled() ? '' : cursor()"
+      [style.backgroundColor]="disabled() ? 'rgb(255, 255, 255)' : bgColor()"
+      [style.color]="disabled() ? 'rgb(0, 0, 0)' : color()"
+      [style.border]="border()"
+      [style.borderRadius.px]="borderRadius()"
+      [style.--hover-translate-y]="disabled() ? '0' : translatey()"
+      [style.--hover-translate-x]="disabled() ? '0' : translatex()"
+      [style.fontSize.rem]="fontSize()"
+      [style.fontWeight]="fontWeight()"
+      [style.boxShadow]="boxShadow()"
+      [style.fontFamily]="fontFamily()"
+      (click)="clicked.emit($event)"
+    >
+      <div class="button-container">
+        <div>{{ icon() }}</div>
+        <div class="text-container">
+          <div>{{ text() }}</div>
+          <div>{{ text1() }}</div>
+        </div>
+      </div>
+    </button>
+  `,
+  styles: `
+
+    button {
+    min-width: max-content;
+    max-width: 15ch;
+    min-height: max-content;
+    padding: 0.25rem 0.25rem;
+    }
+
+    .button-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: row;
+      gap: 1rem;
+    }
+
+      .text-container {
+      display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 0.15rem;
+    }
+
+    button:hover {
+      filter: brightness(1.2); /* Schiarisce del 20% */
+      transition: filter 0.3s ease, transform 0.3s ease; /* Transizione per un effetto più fluido */
+      transform: translateY(var(--hover-translate-y)) translateX(var(--hover-translate-x));
+      
+    }
+    
+    button:active {
+      filter: brightness(1); /* Scurisce del 10% quando premuto */
+    } 
+
+    `,
+})
+export class ButtonComponent {
+  type = input<string>('button');
+  disabled = input<boolean>(false);
+  text = input.required<string, string>({
+    transform: (value: string) => value.toUpperCase(),
+  });
+  text1 = input<string, string>('', {
+    transform: (value: string) => value.toUpperCase(),
+  });
+  icon = input<string>('🎶');
+  width = input<number>(10);
+  height = input<number>(5);
+  cursor = input<string>('pointer');
+  bgColor = input<string>('rgb(131, 131, 131)');
+  color = input<string>('rgb(255, 255, 255)');
+  border = input<string>('1px solid rgb(0, 0, 0)');
+  borderRadius = input<string>('5');
+  cssClass = input<string>('');
+  clicked = output<MouseEvent>();
+  translatey = input<string>('-2px');
+  translatex = input<string>('0');
+  fontSize = input<string>('1.25');
+  fontWeight = input<string>('500');
+  boxShadow = input<string>('0px 2px 4px rgba(0, 0, 0, 0.5)');
+  fontFamily = input<string>('Arial, sans-serif');
+}
